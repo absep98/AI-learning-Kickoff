8d1e410226b98aff7a662e784448d2141ed5f148
Abhishek <absep98@gmail.com>
2026-09-22 16:23:56 +0530

    day30: remove throwaway test scripts, rebuild local RAG to cover all 29 days

--- projects/day-27/awesome-project/test_embeddings.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-vector = model.encode("what is temperature")
-print(len(vector))
-print(vector)
